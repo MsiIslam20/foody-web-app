214bddb9ab3cf4ae5507525c6ec62ad0bc322f2a
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-23 20:36:12 +0600

    updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png'
 import './Header.css';
 
 const Header = () => {
@@ -9,7 +10,7 @@ const Header = () => {
             <nav className="navbar navbar-expand-lg fixed-top">
                 <div className="container">
                     <Link to="/" className="navbar-brand">
-                        <img src="" alt="" className="img-fluid logo"/>
+                        <img src={logo} alt="" className="img-fluid logo"/>
                     </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
