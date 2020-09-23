d659ed050a96742e182853048ae8023ccc93338b
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-23 23:10:20 +0600

    updates

--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import foodData from '../../fakedata/foodData'
 import './Foods.css'
 
 const Foods = () => {
+    const [foods, setFoods] = useState(foodData);
+    console.log(foods);
     return (
         <div>
             <h1>
