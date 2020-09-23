ce1ddbe9a6e85a885289024715359c0a703a098c
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-23 23:40:10 +0600

    updates

--- src/components/FoodItem/FoodItem.js
+++ src/components/FoodItem/FoodItem.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './FoodItem.css'
 
-const FoodItem = () => {
+const FoodItem = (props) => {
+    console.log(props);
+    const {title, subtitle, price, img} = props.food;
     return (
-        <div>
-            
+        <div className="col-md-4">
+            <div className="food-item">
+                <div class="card text-center">
+                    <img src={img} alt="" class="card-img-top" />
+                    <div class="card-body">
+                        <h5>{title}</h5>
+                        <p>{subtitle}</p>
+                        <h4>{price}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import foodData from '../../fakedata/foodData'
+import FoodItem from '../FoodItem/FoodItem';
 import './Foods.css'
 
 const Foods = () => {
     const [foods, setFoods] = useState(foodData);
-    console.log(foods);
     return (
-        <div>
-            <h1>
-                food
-            </h1>
+        <div className="container foods-container">
+            <div className="col-md-12">
+                <ul className="list-unstyled text-center">
+                    <li>Breakfast</li>
+                    <li>Lunch</li>
+                    <li>Dinner</li>
+                </ul>
+                <div className="row">
+                    {
+                        foods.map(food => <FoodItem food={food} key={food.id}></FoodItem>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
