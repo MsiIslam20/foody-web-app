a74401c34546982ed0daef9425b75cfe86ed09f3
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-24 23:50:30 +0600

    updates

---
+++ src/components/Auth/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
+
+const SignUp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/components/Auth/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAovqYGBzMRXVKWVXkabku83i0h8fr9BJI",
+    authDomain: "foody-web-app-f4584.firebaseapp.com",
+    databaseURL: "https://foody-web-app-f4584.firebaseio.com",
+    projectId: "foody-web-app-f4584",
+    storageBucket: "foody-web-app-f4584.appspot.com",
+    messagingSenderId: "607908284267",
+    appId: "1:607908284267:web:c62365dba6b8874dedae2e"
+};
+
+export default firebaseConfig;
