3ee82acf6b021346fd3382d0ff0ab7664729d713
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-26 15:13:48 +0600

    checkout page added

---
+++ src/components/Checkout/Chekout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chekout = () => {
+    return (
+        <div>
+            <h1>This is CheckOut Page</h1>
+        </div>
+    );
+};
+
+export default Chekout;
